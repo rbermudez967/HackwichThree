1d96913cb06b7060dcdd227e4b24d5c817fd63cd
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-02-04 10:49:41 -1000

    initial commit
    parts 1 - 3

---
+++ HackwichThree/HackwichThree/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  HackwichThree
+//
+//  Created by Randy Bermudez on 2/4/21.
+//  Copyright © 2021 Randy Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var firstString = "The background color will turn to blue"
+    var secondString = "The background color will turn to green"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    {
+        //create a conditional statement and uses one of our variables and assigns it to a string - evauluate to false
+        if firstString == "The background color will turn to green"
+        {
+            //if block
+            self.view.backgroundColor = UIColor.red
+        }
+        else //else block is executed because the conditional statement evaluates to false
+        {
+            //else block
+            self.view.backgroundColor = UIColor.blue
+        }
+    }
+    
+    
+}
+
